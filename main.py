1bf4861aaac025c3e66ff473ef65396b93d5504b
Rehan Naim Khan <786iamrehankhan@gmail.com>
2026-03-08 10:02:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -681,4 +681,5 @@ if __name__ == "__main__":
         os.makedirs("templates")
         print("📁 Created templates folder - please add your HTML files")
     
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+uvicorn.run(app, host="0.0.0.0", port=port)
